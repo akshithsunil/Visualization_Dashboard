38498adc80e35d968ca5753bd1e11342c7a0df26
akshithsunil <akshiths100@gmail.com>
2024-06-20 19:35:49 +0530

    added models

--- Dashboard_app/models.py
+++ Dashboard_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DataSource(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    source_url = models.URLField(blank=True, null=True)
+    file_path = models.FileField(upload_to='data_files/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
