9fe33b61f0252331199bbeb3a7152b7850938afe
akshithsunil <akshiths100@gmail.com>
2024-06-20 20:13:06 +0530

    for studying purposes

--- Dashboard_app/views.py
+++ Dashboard_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import DataPoint
 
-# Create your views here.
+
+def index(request):
+    data_points = DataPoint.objects.all()
+    return render(request, 'Dashboard/index.html', {'data_points': data_points})
