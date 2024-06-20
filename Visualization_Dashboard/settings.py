a821a62751b1613cccb10307c6daf6b64a787022
akshithsunil <akshiths100@gmail.com>
2024-06-20 19:51:00 +0530

    added database

--- Visualization_Dashboard/settings.py
+++ Visualization_Dashboard/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Visualization_Dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Dashboard',
+        'USER': 'root',
+        'PASSWORD': 'oneteam',
+        'HOST': 'localhost',
     }
 }
 
